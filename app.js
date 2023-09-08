f9522dc133b357c81e6168c16ea69ad4a167e5c3
Helmut Menhart <helmut.menhart@voestalpine.com>
2023-09-08 12:06:39 +0200

    revert refresh :(

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ cameraTrigger.onclick = function () {
   cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
   cameraOutput.src = cameraSensor.toDataURL("image/webp");
   cameraOutput.classList.add("taken");
-  location.reload();
+  // location.reload();
 };
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
